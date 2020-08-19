54bba083f8b4eb57d6624c0c0a513bd82e61b8f2
bacbartlett <brandonacb@gmail.com>
2020-08-19 17:22:04 -0400

    Finished the basic project

--- game.js
+++ game.js
@@ -1,8 +1,34 @@
 import { getClueFromPromise } from "./promise-version.js";
 import { getClue as getClueFromAsyncFunction } from "./async-await-version.js"
 
+const state = {
+    currentClue: undefined,
+    score: 0
+}
+
+function updateUI(){
+    document.getElementById("score").innerHTML = state.score;
+}
+
+function checkAnswer(){
+    document.getElementById("answer").classList.remove("is-hidden");
+    document.getElementById("check-response").classList.add("is-hidden");
+    const userAnswer = document.getElementById("player-response").value.trim();
+    const answer = state.currentClue.answer.trim();
+    if(userAnswer.toLowerCase() === answer.toLowerCase()){
+        state.score = state.score + Number(state.currentClue.value);
+        document.getElementById("player-response").value = "";
+    }else{
+        state.score = state.score - Number(state.currentClue.value);
+    }
+    updateUI();
+}
 window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("player-response").value = "";
+    document.getElementById("check-response").addEventListener("click", checkAnswer)
     document.getElementById('use-promise').addEventListener('click', function(){
+        document.getElementById("answer").classList.add("is-hidden");
+        document.getElementById("check-response").classList.remove("is-hidden");
         getClueFromPromise().then(data => {
             console.log(data)
             document.getElementById("question").innerHTML = data.question;
@@ -15,10 +41,14 @@ window.addEventListener("DOMContentLoaded", () => {
             } else{
                 invalid.innerHTML = "valid"
             }
+            state.currentClue = data;
         }).catch(error=> console.log(error))
     })
 
     document.getElementById('use-async-await').addEventListener('click' , async function() {
+        document.getElementById("player-response").value = "";
+        document.getElementById("answer").classList.add("is-hidden");
+        document.getElementById("check-response").classList.remove("is-hidden");
         try {
             let data = await getClueFromAsyncFunction();
             document.getElementById("question").innerHTML = data.question;
@@ -31,6 +61,7 @@ window.addEventListener("DOMContentLoaded", () => {
             } else{
                 invalid.innerHTML = "valid"
             }
+            state.currentClue = data;
         } catch (err) {
             console.log(err);
         }
